da2d39280aafe62f12ac234fed4cf05df3d7573d
OlgaKuksa <firsty@tut.by>
2018-09-11 19:13:22 +0300

    added solution including handling larger values

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
-}
+  let regExpMask=/[+-]?\s*\d+/g;
+  let regEx=new RegExp(regExpMask);
+  let parameters=equation.match(regEx).map(elem=>elem.replace(" ",""));
+  if (parameters.length===3) parameters.unshift(1);
+  parameters.splice(1,1);
+  //to be able to handle larger values
+  let average=(Math.abs(parameters[0])+Math.abs(parameters[1])+Math.abs(parameters[2]))/3;
+  let a=parameters[0]/average;
+  let b=parameters[1]/average;
+  let c=parameters[2]/average;
+  const discSqrt=Math.sqrt((Math.pow(b,2)-4*a*c));
+  return [Math.round(-b/2/a-discSqrt/2/a), Math.round(-b/2/a+discSqrt/2/a)].sort((a,b)=>{return a-b})}
